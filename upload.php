a8e012356f8174f2388b8d2808e87ad6c8235a08
Zutno <reviturino@yahoo.com.br>
2016-09-24 22:35:35 -0300

    upload de foto para pasta no servidor

--- upload.php
+++ upload.php
@@ -1,14 +1,21 @@
 <?php
 
 include 'config_upload.inc';
+$i = 0;
+ 
 foreach ($_FILES["arquivos"]["error"] as $key => $error) {
-   
-       $destino = "../fotos/".$_FILES["arquivos"]["name"][$i];
-   
-  
-    move_uploaded_file( $_FILES["arquivos"]["tmp_name"][$i], $destino );
- 
-    #Próximo arquivo a ser analisado
-    $i++;
+   
+    
+    $destino = "fotos/". $_FILES["arquivos"]["name"][$i];
+   
+   
+    move_uploaded_file( $_FILES["arquivos"]["tmp_name"][$i], $destino );
+ 
+    
+    $i++;
+
+
 }
+
+
 ?>
